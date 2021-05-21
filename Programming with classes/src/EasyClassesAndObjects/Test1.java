905c874088d4f057d7aa06553679002da3775751
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-21 09:24:21 +0300

    I started 4th part.

---
+++ Programming with classes/src/EasyClassesAndObjects/Test1.java
@@ -0,0 +1,19 @@
+package EasyClassesAndObjects;
+
+public class Test1 {
+    int firstVariable;
+    int secondVariable;
+
+    private void changeVariableFirst(int firstVariable){
+
+    }
+    private void changeVariableSecond(int secondVariable){
+
+    }
+    private void outputVariables(, int secondVariable){
+        this.firstVariable = firstVariable;
+        this.secondVariable = secondVariable;
+        System.out.println("First variable is " + firstVariable);
+        System.out.println("Second variable is " + secondVariable);
+    }
+}
