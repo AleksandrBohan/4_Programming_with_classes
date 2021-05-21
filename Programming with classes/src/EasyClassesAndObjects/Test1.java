62b649c98c3ef9317bf5a7ff83ba8742e016809f
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-21 09:38:30 +0300

    I have ended task1 from this part.

--- Programming with classes/src/EasyClassesAndObjects/Test1.java
+++ Programming with classes/src/EasyClassesAndObjects/Test1.java
@@ -1,19 +1,47 @@
 package EasyClassesAndObjects;
 
+import java.util.Scanner;
+
 public class Test1 {
-    int firstVariable;
-    int secondVariable;
+   private int firstVariable;
+   private int secondVariable;
+   private Scanner scanner = new Scanner(System.in);
 
-    private void changeVariableFirst(int firstVariable){
-
+    private void changeVariableFirst(){
+        System.out.println("Input value for first variable: ");
+       firstVariable = scanner.nextInt();
     }
-    private void changeVariableSecond(int secondVariable){
-
+    private void changeVariableSecond(){
+        System.out.println("Input value for second variable: ");
+        secondVariable = scanner.nextInt();
     }
-    private void outputVariables(, int secondVariable){
-        this.firstVariable = firstVariable;
-        this.secondVariable = secondVariable;
+    private void outputVariables(){
         System.out.println("First variable is " + firstVariable);
         System.out.println("Second variable is " + secondVariable);
+    }
+
+    private void sumOfVariables(){
+        int sum = firstVariable + secondVariable;
+        System.out.println("sum of variables = " + sum);
+    }
+
+    private void theBiggestValue(){
+        if (firstVariable > secondVariable){
+            System.out.println("The biggest value is " + firstVariable);
+        }
+        else if (firstVariable < secondVariable){
+            System.out.println("The biggest value is " + secondVariable);
+        }
+        else System.out.println("These variables are the same!");
+    }
+
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        test1.changeVariableFirst();
+        test1.changeVariableSecond();
+        test1.outputVariables();
+        test1.sumOfVariables();
+        test1.theBiggestValue();
     }
 }
